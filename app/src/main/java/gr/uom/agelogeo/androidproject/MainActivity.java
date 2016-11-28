465b7d6b0428e45871e4436475a902c3c12cd653
agelogeo (PC) <agelo1995@gmail.com>
2016-11-28 18:49:50 +0200

    Add DatePickers for Departure and Arrival Date

--- app/src/main/java/gr/uom/agelogeo/androidproject/MainActivity.java
+++ app/src/main/java/gr/uom/agelogeo/androidproject/MainActivity.java
@@ -1,13 +1,78 @@
 package gr.uom.agelogeo.androidproject;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import java.util.Calendar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText departureText;
+    EditText arrivalText;
+    int year_x,month_x,day_x;
+    static final int DEPARTURE_DATE_ID = 0;
+    static final int ARRIVAL_DATE_ID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Calendar cal = Calendar.getInstance();
+        year_x=cal.get(Calendar.YEAR);
+        month_x=cal.get(Calendar.MONTH);
+        day_x=cal.get(Calendar.DAY_OF_MONTH);
+
+        showDialogOnClick();
+
     }
+
+    public void showDialogOnClick(){
+        departureText = (EditText) findViewById(R.id.departureDate);
+        departureText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DEPARTURE_DATE_ID);
+            }
+        });
+
+        arrivalText = (EditText) findViewById(R.id.arrivalDate);
+        arrivalText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(ARRIVAL_DATE_ID);
+            }
+        });
+    }
+
+    protected Dialog onCreateDialog(int id){
+        if ( id == DEPARTURE_DATE_ID )
+            return new DatePickerDialog(this, dpickerListner , year_x,month_x,day_x );
+        else if ( id == ARRIVAL_DATE_ID )
+            return new DatePickerDialog(this, dpickerListner2 , year_x,month_x,day_x);
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+           departureText.setText(day+"/"+(month+1)+"/"+year);
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener dpickerListner2
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            arrivalText.setText(day+"/"+(month+1)+"/"+year);
+        }
+    };
 }
