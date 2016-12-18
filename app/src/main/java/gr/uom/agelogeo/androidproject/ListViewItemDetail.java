59986b46e95ed36b02f26aafc205d3e500e661c5
agelogeo (PC) <agelo1995@gmail.com>
2016-12-19 01:14:11 +0200

    Added some Detail adapter for direct flights

--- app/src/main/java/gr/uom/agelogeo/androidproject/ListViewItemDetail.java
+++ app/src/main/java/gr/uom/agelogeo/androidproject/ListViewItemDetail.java
@@ -24,9 +24,7 @@ public class ListViewItemDetail extends AppCompatActivity {
 
         Intent i = this.getIntent();
         String response = i.getStringExtra("JSON_result");
-        int result_ind = i.getIntExtra("LVI_result_ind",0);
         int itinerary_ind = i.getIntExtra("LVI_itinerary_ind",0);
-
 
         try {
             JSONObject result = new JSONObject(response);
@@ -35,11 +33,11 @@ public class ListViewItemDetail extends AppCompatActivity {
             System.out.println(selected_itinerary);
             System.out.println(result.get("fare"));
 
-
-
             JSONObject outbound = selected_itinerary.getJSONObject("outbound");
             JSONArray out_flights = (JSONArray) outbound.get("flights");
 
+            ViewHolder outboundHolder = new ViewHolder();
+            ViewHolder inboundHolder = new ViewHolder();
             if(!selected_itinerary.has("inbound")){
                 LinearLayout inInfo = (LinearLayout) findViewById(R.id.inboundInfo);
                 inInfo.setVisibility(View.GONE);
@@ -53,7 +51,30 @@ public class ListViewItemDetail extends AppCompatActivity {
                     inm1.setVisibility(View.GONE);
                     inm2.setVisibility(View.GONE);
                     inm3.setVisibility(View.GONE);
-                }
+
+                    JSONObject in_flight = in_flights.getJSONObject(0);
+
+                    String originCode = in_flight.getJSONObject("origin").getString("airport");
+                    String destCode = in_flight.getJSONObject("destination").getString("airport");
+
+                    inboundHolder.dDate = (TextView) findViewById(R.id.indDate);
+                    inboundHolder.dDate.setText(in_flight.getString("departs_at"));
+
+                    inboundHolder.dPortCode = (TextView) findViewById(R.id.indPortCode);
+                    inboundHolder.dPortCode.setText(originCode);
+
+                    inboundHolder.dAirline = (TextView) findViewById(R.id.indAirline);
+                    inboundHolder.dAirline.setText(in_flight.getString("operating_airline"));
+
+                    inboundHolder.aDate = (TextView) findViewById(R.id.inaDate);
+                    inboundHolder.aDate.setText(in_flight.getString("arrives_at"));
+
+                    inboundHolder.aPortCode = (TextView) findViewById(R.id.inaPortCode);
+                    inboundHolder.aPortCode.setText(destCode);
+
+                    inboundHolder.aAirline = (TextView) findViewById(R.id.inaAirline);
+                    inboundHolder.aAirline.setText(in_flight.getString("operating_airline"));
+                    }
             }
 
             if(out_flights.length()==1){
@@ -63,13 +84,71 @@ public class ListViewItemDetail extends AppCompatActivity {
                 outm1.setVisibility(View.GONE);
                 outm2.setVisibility(View.GONE);
                 outm3.setVisibility(View.GONE);
+                JSONObject out_flight = out_flights.getJSONObject(0);
+
+                String originCode = out_flight.getJSONObject("origin").getString("airport");
+                String destCode = out_flight.getJSONObject("destination").getString("airport");
+                outboundHolder.journeyView = (TextView) findViewById(R.id.journeyView);
+                outboundHolder.journeyView.setText(originCode+" - "+destCode);
+
+                outboundHolder.dDate = (TextView) findViewById(R.id.outdDate);
+                outboundHolder.dDate.setText(out_flight.getString("departs_at"));
+
+                outboundHolder.dPortCode = (TextView) findViewById(R.id.outdPortCode);
+                outboundHolder.dPortCode.setText(originCode);
+
+                outboundHolder.dAirline = (TextView) findViewById(R.id.outdAirline);
+                outboundHolder.dAirline.setText(out_flight.getString("operating_airline"));
+
+                outboundHolder.aDate = (TextView) findViewById(R.id.outaDate);
+                outboundHolder.aDate.setText(out_flight.getString("arrives_at"));
+
+                outboundHolder.aPortCode = (TextView) findViewById(R.id.outaPortCode);
+                outboundHolder.aPortCode.setText(destCode);
+
+                outboundHolder.aAirline = (TextView) findViewById(R.id.outaAirline);
+                outboundHolder.aAirline.setText(out_flight.getString("operating_airline"));
+
             }
-
-
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public static class ViewHolder{
+        //journeyInfo
+        TextView journeyView;
+        TextView journeyDuration;
+        TextView stopView;
+        //boundInfo
+        TextView Title;
+        //DepartInfo
+        TextView dDate;
+        TextView dPortCode;
+        TextView dPortName;
+        TextView dAirline;
+        //MiddleInfo1
+        TextView m1PortCode;
+        TextView m1PortName;
+        TextView m1Duration;
+        //MiddleInfo2
+        TextView m2PortCode;
+        TextView m2PortName;
+        TextView m2Duration;
+        //MiddleInfo3
+        TextView m3PortCode;
+        TextView m3PortName;
+        TextView m3Duration;
+        //ArriveInfo
+        TextView aDate;
+        TextView aPortCode;
+        TextView aPortName;
+        TextView aAirline;
+
+
+
 
     }
 }
