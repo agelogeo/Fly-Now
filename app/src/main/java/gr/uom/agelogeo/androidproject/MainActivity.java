a6db7c46378533c615cdae1972bef400c77f8aaa
agelogeo (PC) <agelo1995@gmail.com>
2016-12-23 02:23:06 +0200

    Changed icons and menu on navigation drawer

--- app/src/main/java/gr/uom/agelogeo/androidproject/MainActivity.java
+++ app/src/main/java/gr/uom/agelogeo/androidproject/MainActivity.java
@@ -649,17 +649,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_search) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_checkin) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_topdestinations) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_preferences) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_info) {
 
         }
 
